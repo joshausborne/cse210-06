5decf6259f88261bb9e935f8252871a54666075d
Lilly Armenta <78121782+LillyRoxana@users.noreply.github.com>
2022-07-10 20:57:52 -0500

    fixed error

--- batter/game/scripting/load_assets_action.py
+++ batter/game/scripting/load_assets_action.py
@@ -9,7 +9,7 @@ class LoadAssetsAction(Action):
         self._video_service = video_service
 
     def execute(self, cast, script, callback):
-        self._audio_service.load_sounds("ssets/sounds")
+        self._audio_service.load_sounds("assets/sounds")
         self._video_service.load_fonts("assets/fonts")
         self._video_service.load_images("assets/images")
         
